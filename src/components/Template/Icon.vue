3239e9ca3a02213a8a8d94671833ce2055221b3d
DolimovS <samandardolimov71@gmail.com>
2025-08-15 18:05:23 +0500

    first commit

---
+++ src/components/Template/Icon.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-if="iconSvg" v-html="iconSvg"></div>
+</template>
+
+<script>
+    export default {
+        name:"AppIconPage",
+        props:{
+            name:{
+                type:String,
+                required:true,
+            },
+        },
+        data(){
+            return{
+                icons:{
+                    search: `<svg width="24" height="24" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/></svg>`,
+                }
+            }
+        },
+        computed:{
+            iconSvg(){
+                return  this.icons[this.name]|| '';
+            }
+        }
+    }
+</script>
+
+<style  scoped>
+
+</style>
