1dad2d301d2b312c13f3acdaebef84717c4eec4b
DolimovS <samandardolimov71@gmail.com>
2025-08-18 20:32:47 +0500

    font fayl create

---
+++ src/components/HomeSection.vue
@@ -0,0 +1,17 @@
+<template>
+    <div >
+        <div class="HomeSection">
+
+        </div>
+    </div>
+</template>
+
+<script>  
+    export default {
+        name:"HomeSection",
+    }
+</script>
+
+<style scoped>
+
+</style>
