b71c2b25557e7fc28aa88b1f9ddff54eb3c21784
DolimovS <samandardolimov71@gmail.com>
2025-08-21 00:49:22 +0500

    devisec add

--- src/components/Devices.vue
+++ src/components/Devices.vue
@@ -1,14 +1,25 @@
 <template>
     <div>
-
+            <CardDevices :card_text="card_text" :card_discription="card_discription"/>
     </div>
 </template>
 
 <script>
+import CardDevices from './CardDevices.vue';
+
 
     export default {
-        name:"Devices"
-        
+
+        name:"Devices",
+        components:{
+            CardDevices,
+        },
+        data(){
+            return{
+                card_text:"Smartphones",
+                card_discription:"StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
+            }
+        }
     }
 </script>
 
