125e78b98aa10e54067c3081cf09bb024bef47f1
DolimovS <samandardolimov71@gmail.com>
2025-08-21 21:10:48 +0500

    add question and media

--- src/components/Devices.vue
+++ src/components/Devices.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-            <CardDevices :card_text="card_text" :card_discription="card_discription"/>
+    <div class="cards_container" >
+            <CardDevices v-for="(title,index) in card_product" :key="index"
+            :card_text="title.card_text" 
+            :card_discription="title.card_discription"/>
     </div>
 </template>
 
@@ -16,13 +18,54 @@ import CardDevices from './CardDevices.vue';
         },
         data(){
             return{
-                card_text:"Smartphones",
-                card_discription:"StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
+                card_product:[
+                    {
+                        id: 1,
+                        card_text: "Smartphones",
+                        card_discription: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
+                    },
+                    {
+                        id: 2,
+                        card_text: "Smartphones",
+                        card_discription: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
+                    },
+                    {
+                        id: 3,
+                        card_text: "Smartphones",
+                        card_discription: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
+                    },
+                    {
+                        id: 4,
+                        card_text: "Smartphones",
+                        card_discription: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
+                    },
+                    {
+                        id: 5,
+                        card_text: "Smartphones",
+                        card_discription: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
+                    },
+                    {
+                        id: 6,
+                        card_text: "Smartphones",
+                        card_discription: "StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store"
+                    }
+            ]
             }
         }
     }
 </script>
 
 <style >
+.cards_container{
+    display: flex;
+    flex-wrap: wrap;
+    gap: 30px;
+}
 
+
+@media (max-width:1440px) {
+.cards_container{
+    gap: 20px;
+}
+}
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,19 @@
 <template>
-   <div class="home">
-     <HomeSection/>
-    <HomeCatogoriya/>
-    <div class="decices_product">
-        <Sarlavha  :title="devices_title" :description="devices_description"/>
-        <Devices />
+    <div class="home">
+        <HomeSection />
+        <HomeCatogoriya />
+        <div class="decices_product">
+            <Sarlavha :title="devices_title" :description="devices_description" />
+            <Devices />
+        </div>
+        <div class="questions_page">
+            <div class="question_title">
+                <Sarlavha :title="questions_title" :description="questions_description" />
+                <Button :btn_title="btn_title"
+                 btn_bc_color="#E50000"/>
+            </div>
+        </div>
     </div>
-   </div>
 </template>
 
 <script>
@@ -14,40 +21,70 @@ import HomeCatogoriya from '../components/HomeCatogoriya.vue';
 import HomeSection from '../components/HomeSection.vue';
 import Sarlavha from '../components/Sarlavha.vue';
 import Devices from "../components/Devices.vue"
+import Button from "../components/Button.vue"
 
 
-    export default {
-        name:"home",
-        components:{
-            HomeSection,
-            HomeCatogoriya,
-            Sarlavha,
-            Devices,
-        },
-        data(){
-            return{
-                devices_title:"We Provide you streaming experience across various devices.",
-                devices_description:"With StreamVibe, you can enjoy your favorite movies and TV shows anytime, anywhere. Our platform is designed to be compatible with a wide range of devices, ensuring that you never miss a moment of entertainment.",
-            }
+export default {
+    name: "home",
+    components: {
+        HomeSection,
+        HomeCatogoriya,
+        Sarlavha,
+        Devices,
+        Button,
+    },
+    data() {
+        return {
+            devices_title: "We Provide you streaming experience across various devices.",
+            devices_description: "With StreamVibe, you can enjoy your favorite movies and TV shows anytime, anywhere. Our platform is designed to be compatible with a wide range of devices, ensuring that you never miss a moment of entertainment.",
+            questions_title:"Frequently Asked Questions",
+            questions_description:"Join StreamVibe and select from our flexible subscription options tailored to suit your viewing preferences. Get ready for non-stop entertainment!",
+
+            btn_title:"Ask a Question",
         }
     }
+}
 </script>
 
-<style >
-
-.home{
+<style>
+.home {
     padding: 0 10px;
     max-width: 1600px;
     margin: auto;
-display: flex;
-flex-direction: column;
-/* gap: 150px; */
-}
-.decices_product{
     display: flex;
     flex-direction: column;
-    padding-top: 150px;
-    gap: 80px;
+    gap: 150px;
 }
 
+.decices_product {
+    display: flex;
+    flex-direction: column;
+    gap: 80px;
+}
+/* questions_page */
+.questions_page{
+
+}
+.question_title{
+    display: flex;
+    justify-content: space-between;
+    align-items: end;
+    gap: 10px;
+}
+
+@media (max-width:1440px) {
+    .decices_product {
+        padding-top: 120px;
+        gap: 60px;
+    }
+}
+
+@media (max-width:600px) {
+.question_title{
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: 20px;
+}
+}
 </style>
